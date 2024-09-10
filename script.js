36b836a4c15db71d37b17e5083988ddfc7e35724
ozan1535 <ozanbilgic1535@gmail.com>
2024-09-10 19:25:38 +0300

    conditional statements

---
+++ script.js
@@ -0,0 +1,51 @@
+/* let isRaining = false;
+
+if (isRaining) {
+  console.log("Take an umbrella");
+} */
+
+/* let weather = "raining";
+
+if (weather === "raining") {
+  console.log("Take an umbrella");
+} else if (weather === "sunny") {
+  console.log("Wear sunglasses");
+} */
+
+/* let weather = "cloudy";
+
+if (weather === "raining") {
+  console.log("Take an umbrella");
+} else if (weather === "sunny") {
+  console.log("Wear sunglasses");
+} else if (weather === "cloudy") {
+  console.log("The weather is cloudy");
+} else {
+  console.log("Stay home");
+}
+ */
+
+/* if (true) {
+  console.log("This statement will be shown");
+} else if (false) {
+  console.log("Another statement will be shown");
+} else {
+  console.log("This statement is for false");
+} */
+
+let trafficLight = "purple";
+
+switch (trafficLight) {
+  case "red":
+    console.log("Stop");
+    break;
+  case "yellow":
+    console.log("Get ready");
+    break;
+  case "green":
+    console.log("Go");
+    break;
+  default:
+    console.log("Invalid color");
+    break;
+}
